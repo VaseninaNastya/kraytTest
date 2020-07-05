be8366695dcd721cc7b53c5a2c4c6e31c6014936
NastyaVasenina <amedeo_@mail.ru>
2020-07-05 21:14:42 +0300

    Добавила стайдер на блок screenshots

--- src/main.js
+++ src/main.js
@@ -23,5 +23,84 @@ $(document).ready(function () {
         },
     ],
     });
+    var  carousel = $("#carousel").waterwheelCarousel({
+        flankingItems: 3,
+        separationMultiplier: 0.7,
+        opacityMultiplier: 1,
+        responsive: [
+            {
+            breakpoint: 1025,
+            settings: {
+                flankingItems: 2,
+            },
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                flankingItems: 1,
+            },
+            },
+        ],
+    });
+    
+    $('#prev').bind('click', function () {
+        carousel.prev();
+        return false
+    });
+    
+    $('#next').bind('click', function () {
+        carousel.next();
+        return false;
+    });
 });
 
+
+
+jQuery(window).on('load resize', function () {
+    if (jQuery(window).width() < 1024) {
+        $(document).ready(function () {
+            var  carousel = $("#carousel").waterwheelCarousel({
+                flankingItems: 1,
+                separationMultiplier: 0.7,
+                opacityMultiplier: 1,
+            });
+            $('#prev').bind('click', function () {
+                carousel.prev();
+                return false
+            });
+            $('#next').bind('click', function () {
+                carousel.next();
+                return false;
+            });
+        });
+        
+    }
+    else if (jQuery(window).width() < 1024) {
+        $(document).ready(function () {
+            var  carousel = $("#carousel").waterwheelCarousel({
+                flankingItems: 3,
+                separationMultiplier: 0.7,
+                opacityMultiplier: 1,
+            });
+            $('#prev').bind('click', function () {
+                carousel.prev();
+                return false
+            });
+            $('#next').bind('click', function () {
+                carousel.next();
+                return false;
+            });
+        });
+    }
+    });
+
+
+    //$('#prev').bind('click', function () {
+      //  carousel.prev();
+        //return false
+    //});
+   // 
+    //$('#next').bind('click', function () {
+      //  carousel.next();
+        //return false;
+    //});
