235ce489a6abd5689353dda64bb111650e8a9830
NastyaVasenina <amedeo_@mail.ru>
2020-07-15 20:50:48 +0300

    Окончательно настроила карусель скриншотов, добавила адаптивность

--- src/main.js
+++ src/main.js
@@ -23,76 +23,162 @@ $(document).ready(function () {
         },
     ],
     });
-    var  carousel = $("#carousel").waterwheelCarousel({
+});
+
+var carousel = null;
+
+console.log(document.documentElement.clientWidth);
+$(document).ready(function (){
+    console.log("большая карусель")
+    carousel = $("#carousel").waterwheelCarousel({
         flankingItems: 3,
         separationMultiplier: 0.7,
         opacityMultiplier: 1,
-        responsive: [
-            {
-            breakpoint: 1025,
-            settings: {
-                flankingItems: 2,
-            },
-            },
-            {
-            breakpoint: 480,
-            settings: {
-                flankingItems: 1,
-            },
-            },
-        ],
+        opacityMultiplier: 1,
+        sizeMultiplier:0.9
     });
-    
+})
+
+$(window).on('load resize', function(){
+    console.log(document.documentElement.clientWidth);
+    if (document.documentElement.clientWidth < 1024) {
+        carousel.reload({
+            flankingItems: 1,
+            separationMultiplier: 0.7,
+            opacityMultiplier: 1,
+            opacityMultiplier: 1,
+            sizeMultiplier:0.7
+        })
+    } else {
+        carousel.reload({
+            flankingItems: 3,
+            separationMultiplier: 0.8,
+            opacityMultiplier: 1,
+            opacityMultiplier: 1,
+            sizeMultiplier:0.9
+        })
+    }
     $('#prev').bind('click', function () {
         carousel.prev();
         return false
-    });
     
+    });
     $('#next').bind('click', function () {
         carousel.next();
         return false;
     });
-});
+    
+})
 
 
+/////////////////////
+/*
+jQuery(window).on('load resize', function () {
+    
+$(document).ready(function () {
+    if (document.documentElement.clientWidth > 1024){
+        var  carousel = null
+        carousel = $("#carousel").waterwheelCarousel({
+            flankingItems: 3,
+            separationMultiplier: 0.7,
+            opacityMultiplier: 1,
+            opacityMultiplier: 1,
+            sizeMultiplier:0.9
+        });
+        console.log("большая карусель");
+        
+    }
+    else{
+        carousel = $("#carousel").waterwheelCarousel({
+            flankingItems: 1,
+            separationMultiplier: 0.7,
+            opacityMultiplier: 1,
+            opacityMultiplier: 1,
+            sizeMultiplier:0.9
+        });
+        console.log("маленькая карусель");
+}
+    $('#prev').bind('click', function () {
+        carousel.prev();
+        console.log(carousel)
+        return false
+
+    });
+    $('#next').bind('click', function () {
+        carousel.next();
+        return false;
+    });
+})
+})
+*/
+/////////////////////
+
+/*
+jQuery(window).on('load resize', function () { 
+    if (document.documentElement.clientWidth < 1024){
+        $(document).ready(function () {
+            var  carousel = $("#carousel").waterwheelCarousel({
+                flankingItems: 1,
+                separationMultiplier: 0.7,
+                opacityMultiplier: 1,
+                sizeMultiplier: 0.9,
+            });
+        console.log("первый");
+        });
+    }
+    else {
+        $(document).ready(function () {
+            var  carousel = $("#carousel").waterwheelCarousel({
+                flankingItems: 3,
+                separationMultiplier: 0.7,
+                opacityMultiplier: 1,
+                opacityMultiplier: 1,
+                sizeMultiplier:0.9
+            });
+            console.log("второй");
+        });
+    }
+    });
+/*
 
 jQuery(window).on('load resize', function () {
-    if (jQuery(window).width() < 1024) {
+    if (document.documentElement.clientWidth < 1024) {
         $(document).ready(function () {
             var  carousel = $("#carousel").waterwheelCarousel({
                 flankingItems: 1,
                 separationMultiplier: 0.7,
                 opacityMultiplier: 1,
             });
-            $('#prev').bind('click', function () {
-                carousel.prev();
-                return false
-            });
-            $('#next').bind('click', function () {
-                carousel.next();
-                return false;
-            });
+        });
+        $('#prev').bind('click', function () {
+            carousel.prev();
+            return false
+        });
+        $('#next').bind('click', function () {
+            carousel.next();
+            return false;
         });
         
     }
-    else if (jQuery(window).width() < 1024) {
+    else if (document.documentElement.clientWidth > 1024) {
         $(document).ready(function () {
             var  carousel = $("#carousel").waterwheelCarousel({
                 flankingItems: 3,
                 separationMultiplier: 0.7,
                 opacityMultiplier: 1,
             });
-            $('#prev').bind('click', function () {
-                carousel.prev();
-                return false
-            });
-            $('#next').bind('click', function () {
-                carousel.next();
-                return false;
-            });
+        });
+        $('#prev').bind('click', function () {
+            carousel.prev();
+            return false
+        });
+        $('#next').bind('click', function () {
+            carousel.next();
+            return false;
         });
     }
     });
+*/
 
 
     //$('#prev').bind('click', function () {
@@ -104,3 +190,6 @@ jQuery(window).on('load resize', function () {
       //  carousel.next();
         //return false;
     //});
+
+
+    
